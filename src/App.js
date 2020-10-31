fa0d8c38fef7f755f3e423e3f335c6ef91c0ff87
Daniel Soto <dasg.1993@gmail.comm>
2020-10-30 20:36:31 -0500

    [featue] - create Edit Employee & add in router

--- src/App.js
+++ src/App.js
@@ -5,13 +5,14 @@ import './stylesheet/style.css'
 import {Home} from './components/Home'
 import { AddEmployee } from './components/AddEmployee'
 import { GlobalProvider } from './context/GlobalState'
-
+import { EditEmployee } from './components/EditEmployee'
 const App = () => {
   return (
     <GlobalProvider>
       <Switch>
         <Route path="/" component={Home} exact />
         <Route path="/add" component={AddEmployee} exact />
+        <Route path="/edit/:id" component={EditEmployee} exact />
       </Switch>
     </GlobalProvider>
   );
