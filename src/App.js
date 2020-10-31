3a4b3d619ec110dd4995b29f4439dcc449a92729
Daniel Soto <dasg.1993@gmail.comm>
2020-10-30 20:13:45 -0500

    [featue] - creatue add employee & employee list component & add in route

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Switch } from 'react-router-dom'
 import './stylesheet/tailwind.output.css'
 import './stylesheet/style.css'
 import {Home} from './components/Home'
+import { AddEmployee } from './components/AddEmployee'
 import { GlobalProvider } from './context/GlobalState'
 
 const App = () => {
@@ -10,6 +11,7 @@ const App = () => {
     <GlobalProvider>
       <Switch>
         <Route path="/" component={Home} exact />
+        <Route path="/add" component={AddEmployee} exact />
       </Switch>
     </GlobalProvider>
   );
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import {Heading} from './Heading'
+import  { EmployeeList } from './EmployeeList'
 
 
 export const Home = () => {
@@ -9,6 +10,7 @@ export const Home = () => {
                 <div className="container mx-auto">
                     <h3 className="text center text-3xl mt-20 text-base leading-8 text-black font-bold tracking-wide uppercase">CRUD - Context API and Hook React </h3>
                     <Heading />
+                    <EmployeeList />
                 </div>
             </div>
         </>
