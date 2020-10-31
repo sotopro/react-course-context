b180e452fbfef821e0f12119f0915b1f03d1bf68
Daniel Soto <dasg.1993@gmail.comm>
2020-10-30 19:12:53 -0500

    [feature] - creatue Home component & setup route & browserRoute in components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Route, Switch } from 'react-router-dom'
+import './stylesheet/tailwind.output.css'
+import './stylesheet/style.css'
+import {Home} from './components/Home'
+import { GlobalProvider } from './context/GlobalState'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <GlobalProvider>
+      <Switch>
+        <Route path="/" component={Home} exact />
+      </Switch>
+    </GlobalProvider>
   );
 }
 
---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {Heading} from './Heading'
+
+
+export const Home = () => {
+    return (
+        <>
+            <div className="App">
+                <div className="container mx-auto">
+                    <h3 className="text center text-3xl mt-20 text-base leading-8 text-black font-bold tracking-wide uppercase">CRUD - Context API and Hook React </h3>
+                    <Heading />
+                </div>
+            </div>
+        </>
+    )
+}
